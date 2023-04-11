8269af6c1b44160257dababa57321dc97d55d4fa
nicholasysf <nicholasysf@gmail.com>
2023-04-11 13:03:58 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+
+//Setting up config.env file variables
+dotenv.config({path : './config/config.env'});
+
+
+
+
+const PORT = process.env.PORT
+app.listen(PORT,()=>{
+    console.log(`Server started at ${PORT}`);
+});
+
+
+
+
